1396e115daad25f5459f9e5cbae337f8f18974f4
Lobo <andreas.dahm@gmail.com>
2025-05-02 11:26:01 +0200

    Added new style and styled login and register page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import { GameRegisterComponent } from './components/game-register/game-register.component';
-import { AuthComponent } from './components/auth/auth.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule 
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
-import { GameRegisterComponent } from './components/game-register/game-register.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
 
@@ -10,11 +9,11 @@ export const routes: Routes = [
         path: 'register',       // <-- Path to find the component (z.B. http://localhost:4200/register)
         component: AuthComponent // <-- Matching component
     },
-    {
+    /*{
         path: 'home',       // <-- Path to find the component (z.B. http://localhost:4200/home)
         component: GameRegisterComponent, // <-- Matching component
         canActivate: [AuthGuard]
-    },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: '**', redirectTo: '/home' }
+    },*/
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/login' }
 ];
